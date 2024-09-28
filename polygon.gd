ee85bb9453fe850af582358d6438f487347ad93a
TheBogdichHD <nottoowellconstructed@gmail.com>
2024-09-28 20:46:56 +0300

    affine translation added

--- polygon.gd
+++ polygon.gd
@@ -3,7 +3,9 @@ extends Control
 @onready var dx_selector: LineEdit = $VBoxContainer/MarginContainer/MenuPanel/DxSelector
 @onready var dy_selector: LineEdit = $VBoxContainer/MarginContainer/MenuPanel/DySelector
 
+var T: DenseMatrix = DenseMatrix.identity(3)
 var polygon: Array[Vector2]
+
 
 func _draw() -> void:
 	var color = Color.from_string("FCFAEE", Color.WHITE)
@@ -15,24 +17,37 @@ func _draw() -> void:
 			old_point = point
 		draw_line(old_point, first_point, color)
 
-func _input(event: InputEvent) -> void:
+
+func _on_panel_container_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if event.button_index == 1:
-			polygon.append(event.position)
+		if event.button_index == 1 and event.pressed:
+			polygon.append(event.global_position)
 			queue_redraw()
-		
+
 
 func _on_clear_pressed() -> void:
 	polygon.clear()
 	queue_redraw()
 
 func affine_move(point, dx, dy):
-	var T: DenseMatrix = DenseMatrix.identity(3)
+	T.set_element(2, 0, dx)
+	T.set_element(2, 1, dy)
 	
+	var V: DenseMatrix = DenseMatrix.from_packed_array([1, 1, 1], 1, 3)
+	V.set_element(0, 0, point.x)
+	V.set_element(0, 1, point.y)
+	
+	V = V.multiply_dense(T)
+	
+	return Vector2(V.get_element(0, 0), V.get_element(0, 1))
+
 func _on_move_pressed() -> void:
 	if dx_selector.text == "" or dy_selector.text == "":
 		return
-	var dx = int(dx_selector.text)
-	var dy = int(dx_selector.text)
+	
+	var dx = float(dx_selector.text)
+	var dy = float(dx_selector.text)
 	for i in range(polygon.size()):
 		polygon[i] = affine_move(polygon[i], dx, dy)
+	
+	queue_redraw()
